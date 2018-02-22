e53eb5607182084060e897592458c09dfaefc259
pierrick <pierrick.dannoville@gmail.com>
2018-02-22 19:42:34 +0100

    working

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   get 'polyvalent', to:'home#polyvalent'
 
-  get 'mairie', to:'home/mairies'
+  get 'mairies', to:'home#mairies'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
